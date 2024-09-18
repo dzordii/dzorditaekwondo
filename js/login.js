b181f0b69eafa2fd68ee40a3b825e5fb83d75b9c
Isaac Dzordi <dzordinetwork@gmail.com>
2024-09-17 23:16:16 -0300

    passwords

--- js/login.js
+++ js/login.js
@@ -1,11 +1,29 @@
 document.getElementById('loginBtn').addEventListener('click', function() {
     // Array de usuários com nome e senha
     const users = [
-        { username: 'aluno', password: 'aluno123' },
         { username: 'admin', password: 'admin' },
-        { username: 'aluno2', password: 'abcdef' },
-        { username: 'aluno3', password: '7891011' },
-        { username: 'aluno4', password: 'senha123' }
+        { username: 'leomatos', password: 'branca123' },
+        { username: 'maria', password: 'laranja123' },
+        { username: 'felipe', password: 'pontaverde123' },
+        { username: 'guifontella', password: 'amarela123' },
+        { username: 'matheus', password: 'pontaverde123' },
+        { username: 'mateus', password: 'laranja123' },
+        { username: 'samuel', password: 'laranja123' },
+        { username: 'belly', password: 'pontaverde123' },
+        { username: 'davi', password: 'laranja123' },
+        { username: 'chaveiro', password: 'laranja123' },
+        { username: 'luiz', password: 'laranja123' },
+        { username: 'yure', password: 'branca123' },
+        { username: 'isaac', password: 'laranja123' },
+        { username: 'bryan2', password: 'laranja123' },
+        { username: 'alana', password: 'laranja123' },
+        { username: 'lucasmagnus', password: 'laranja123' },
+        { username: 'lucascardoso', password: 'pontaverde123' },
+        { username: 'thalita', password: 'pontaverde123' },
+        { username: 'arthur', password: 'pontaverde123' },
+        { username: 'tiago', password: 'pontapreta123' },
+        { username: 'pedro', password: 'pontaverde123' },
+        { username: 'bryan', password: 'verde123' }
     ];
 
     // Pega o valor dos campos de entrada
